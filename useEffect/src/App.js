334f3e5b8b80ca20076cab2c8951ed36de69d13a
DESKTOP-NAK2DKH\Junsu <motors3996@gmail.com>
2022-08-09 23:39:46 +0900

    ADD : useEffect

--- useEffect/src/App.js
+++ useEffect/src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
-import Time from './components/Time';
+import React,{useState} from 'react';
+import Time from './components/Time'
 
 function App() {
-  return <Time />
+  const [showTime,setShowTime] = useState(false);
+
+  return <div>
+    {showTime && <Time showTime={showTime}/>}
+    <button onClick={()=>{setShowTime(!showTime)}}>Time Toggle</button>
+  </div>
 }
 
 export default App;
--- useEffect/src/components/Time.js
+++ useEffect/src/components/Time.js
@@ -1,5 +1,32 @@
-function Time() {
-  return <div></div>
-}
+import React,{useEffect, useState} from "react";
 
-export default Time;
+export default function Time (props) {
+
+  const [clock, setClock] = useState("00시00분00초");
+  const [day, setDay] = useState("0000년00월00일");
+  
+  useEffect(()=>{
+      let time = new Date();
+      setDay(time.getFullYear()+"년"+(time.getMonth()+1)+"월"+time.getDate()+"일");
+      console.log("날짜가 설정되었습니다.")
+  },[props.showTime])
+
+  useEffect(()=> {
+    const Timer = setInterval(() => {
+      let time = new Date();
+      setClock(time.getHours()+"시"+time.getMinutes()+"분"+time.getSeconds()+"초");
+    },1000);
+    console.log("mount!");
+    return ()=>{
+       clearInterval(Timer);
+       console.log("unmount!")
+    }
+
+  },[]);
+
+  return <div>
+    <p>{day}</p>
+    <p>{clock}</p>
+    <p>Time Mount Success!</p>
+  </div>
+};
